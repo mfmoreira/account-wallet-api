f251232eead765f3f06b64dc1ea26ab520f95175
mfmoreira <mfmoreira7@gmail.com>
2022-02-14 11:19:28 -0300

    Add service async

--- src/main/java/com/picpay/wallet/service/EntryService.java
+++ src/main/java/com/picpay/wallet/service/EntryService.java
@@ -5,6 +5,7 @@ import java.time.LocalDateTime;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -63,7 +64,7 @@ public class EntryService {
 		throw new AccountNotFoundException(entryRequest.getAccount().getId());
 	}
 
-	@Transactional
+	@Async
 	public void transfer(TransferRequest transferRequest) throws InsufficientFundsException {
 		Entry entryOrigin = new Entry();
 		Entry entryDestination = new Entry();
